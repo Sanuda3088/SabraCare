2dd8a0d76c7c70f7da3397784108c5f249ff0eb5
Sanuda3088 <sanudasenod446@gmail.com>
2024-07-17 11:06:40 +0530

    "Erroe solved keep up the work"

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -4,7 +4,7 @@ import { Link,useNavigate } from "react-router-dom";
 import { BASE_URL } from "../config";
 import { toast } from 'react-toastify';
 import { authContext } from '../context/AuthContext.jsx';
-import HashLoader from "react-spinners/HashLoader"
+import HashLoader from "react-spinners/HashLoader.js"
 
 const Login = () => {
   const [loading, setLoading] = useState(false);
