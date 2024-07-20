0b9e50d6343e4a0bb9102d48807f7847afb76929
ZulfaZulfikar <sulfafathima11@gmail.com>
2024-07-21 01:31:05 +0530

    Services and Feature done

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,6 +8,8 @@ import icon02 from "../assets/images/icon02.png";
 import icon03 from "../assets/images/icon03.png";
 import {Link} from "react-router-dom";
 import {BsArrowRight} from 'react-icons/bs'
+import ServiceList from '../Components/Services/ServiceList'
+
 
 const Home = () => {
   return (
@@ -130,6 +132,76 @@ const Home = () => {
           </div>
         </div>
       </section>
+
+       {/* Services Section Start*/}
+    <section>
+      <div className="container">
+        <div className='xl:w-[470px] mx-auto'>
+          <h2 className='heading text-center'>Our medical services</h2>
+          <p className='text__para text-center'>World-class care for everyone. Our health System offers unmatched, expert health care.</p>
+        </div>
+        <ServiceList />
+      </div>
+    </section>
+
+    {/* Services Section End*/}
+
+       {/* Feature-section strat */}
+
+       <section>
+      <div className="container">
+        <div className="flex items-center justify-between flex-col lg:flex-row">
+
+          {/* Feature content */}
+            <div className='xl:w-[670px]'>
+              <h2 className="heading">Get virtual treatment <br />anytime.</h2>
+
+              <ul className="pl-4">
+                <li className="text__para">1. Schedule the appointment directly.</li>
+                <li className="text__para">2. Search for your physician here, and contact their office.</li>
+                <li className="text__para">3. View our physicians who are accepting new patients, use the online scheduling tool to select an appointment time.</li>
+              </ul>
+              <Link to='/'>
+              <button className="btn">Learn More</button>
+              </Link>
+            </div>
+
+            {/* feature image */}
+
+            <div className='relative z-10 xl:w-[770px] flex justify-end mt-[50px] lg:mt-0'>
+              <img src={featureImg} className='w-3/4' alt="" />
+
+              <div className='w-[150px] lg:w-[248px] bg-white absolute bottom-[50px] left-0 md:bottom-[100px] md:left-5 z-20 p-2 pb-3 lg:pt-4 lg:px-4 lg:pb-[26px] rounded-[10px]'>
+                <div className='flex items-center justify-between'>
+                  <div className='flex items-center gap-[6px] lg:gap-3'>
+                    <p className='text-[10px] leading-[10px] lg:text-[14px] lg:leading-5 text-headingColor font-[600]'>
+                      Tue, 24
+                    </p>
+                    <p className='text-[10px] leading-[10px] lg:text-[14px] lg:leading-5 text-textColor font-[400]'>
+                      10:00AM
+                    </p>
+                  </div>
+                  <span className='w-5 h-5 lg:w-[34px] lg:h-[34px] flex items-center justify-center bg-yellowColor rounded py-1 px-[6px] lg:py-3 lg:px-[9px]'>
+                    <img src={videoIcon} alt="" />
+                  </span>
+                </div>
+
+                <div className='w-[65px] lg:w-[96px] bg-[#CCF0F3] py-1 px-2 lg:py-[6px] lg:px-[10px] text-[8px] leading-[8px] lg:text-[12px] lg:leading-4 text-irisBlueColor font-[500] mt-2 lg:mt-4 rounded-full'>
+                  Consultation
+                </div>
+                <div className='flex items-center gap-[6px] lg:gap-[10px] mt-2 lg:mt-[18px]'>
+                  <img src={avatarIcon} alt="" />
+                  <h4 className="text-[10px] leading-3 lg:text-[16px] lg:leading-[22px] font-[700] text-headingColor">Wayne Collins</h4>
+
+                </div>
+              </div>
+            </div>
+
+        </div>
+      </div>
+    </section>
+
+    {/* Feature-section end */}
     </>
   );
 };
