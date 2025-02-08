2a90064219bb4b9d83a4489b6509ad0feafd2f3a
Sanuda Senod <ypsspathiraja@std.appsc.sab.ac.lk>
2025-02-08 16:57:19 +0530

    untill 44.45 minutes done in the 5th

--- frontend/src/pages/Doctors/DoctorAbout.jsx
+++ frontend/src/pages/Doctors/DoctorAbout.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import { dateFormat } from '../../../utils/dateFormat';
 
-const DoctorAbout = () => {
+
+const DoctorAbout = ({name, about, qualifications=[], experiences=[]}) => {
   return (
     <div>
       <div>
         <h3 className="text-[20px] leading-[30px] text-headingColor font-semibold flex items-center gap-2">
           About of
           <span className="text-irisBlueColor font-bold text-[24px] leading-9">
-           Dr. Sanuda Senod
+           Dr. Sanuda Senod{name}
           </span>
         </h3>
         <p className="text_para">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
                     ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
                     esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum. {about}</p>
       </div>
 
       <div className="mt-12">
@@ -24,7 +25,7 @@ const DoctorAbout = () => {
         </h3>
 
         <ul className="pt-4 md:p-5">
-          {/*qualifications.map((item, index) => (
+          {qualifications.map((item, index) => (
             <li
               key={index}
               className="flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px] border-2 border-irisBlueColor p-3 rounded"
@@ -42,7 +43,7 @@ const DoctorAbout = () => {
                 Sabaragamuwa University
               </p>
             </li>
-          ))*/}
+           ))}
         </ul>
       </div>
 
@@ -52,7 +53,7 @@ const DoctorAbout = () => {
         </h3>
 
         <ul className="grid sm:grid-cols-2 gap-[30px] pt-4 md:p-5">
-          {/*experiences.map((item, index) => (
+          {experiences.map((item, index) => (
             <li key={index} className="p-4 rounded-[10px] bg-[#fff9ea]">
               <span className="text-yellowColor text-[15px] leading-6 font-semibold">
                 {formateDate(item.startingDate)} - {formateDate(item.endingDate)}
@@ -65,7 +66,7 @@ const DoctorAbout = () => {
                 Lanka Hospital
               </p>
             </li>
-          ))*/}
+         ))} 
         </ul>
       </div>
     </div>
