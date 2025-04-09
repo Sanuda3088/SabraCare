859c4ec1ea12f25f6580c25dca2884ab27321a50
Sanuda Senod <ypsspathiraja@std.appsc.sab.ac.lk>
2025-04-09 15:55:45 +0530

    confg file changed

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 // locally
-export const BASE_URL = "http://localhost:5000/api/v1";
+// export const BASE_URL = "http://localhost:5000/api/v1";
 
 // on production
-// export const BASE_URL = "https://api.mydomain.com/api/v1";
+export const BASE_URL = "https://sabracarebackend.onrender.com/api/v1";
 export const token = localStorage.getItem("token");
