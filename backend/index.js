bec158ef7674400df28acc5125dd71b06c319351
Sanuda Senod <ypsspathiraja@std.appsc.sab.ac.lk>
2025-04-09 13:25:01 +0530

    cors policy and base url switching codes added

--- backend/index.js
+++ backend/index.js
@@ -49,7 +49,7 @@ const connectDB = async () => {
 //middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 app.use('/api/v1/auth', authRoute);
 app.use('/api/v1/users', userRoute);
 app.use('/api/v1/doctors', doctorRoute);
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,6 @@
+// locally
 export const BASE_URL = "http://localhost:5000/api/v1";
+
+// on production
+// export const BASE_URL = "https://api.mydomain.com/api/v1";
 export const token = localStorage.getItem("token");
